df703104a21fa8c068f79cf60f89ccc16b7ad381
jfarrell720 <jamesfarrell720@gmail.com>
2025-01-31 14:09:16 -0500

    uploading email

--- src/app/contactme/contactme.component.ts
+++ src/app/contactme/contactme.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
+import { FormsModule } from '@angular/forms'; // Import FormsModule here
 
 @Component({
   selector: 'app-contactme',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contactme.component.html',
-  styleUrl: './contactme.component.scss'
+  styleUrls: ['./contactme.component.scss']
 })
 export class ContactmeComponent {
+  public sendEmail(e: Event) {
+    e.preventDefault();
 
+    emailjs
+      .sendForm('service_4uogd5r', 'template_ecnoiey', e.target as HTMLFormElement, {
+        publicKey: 'aZH5_9HUvSDxayVvW',
+      })
+      .then(
+        () => {
+          alert('Success! Your message has been sent.');
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          alert('Failed to send the message. Please try again later.');
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+        },
+      );
+  }
 }
