97c35e3b59ee33337b33fc71dcfcc86952aa1b91
jfarrell720 <jamesfarrell720@gmail.com>
2025-01-31 14:14:02 -0500

    updated

--- src/app/contactme/contactme.component.ts
+++ src/app/contactme/contactme.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
-import { FormsModule } from '@angular/forms'; // Import FormsModule here
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contactme',
@@ -9,13 +9,25 @@ import { FormsModule } from '@angular/forms'; // Import FormsModule here
   styleUrls: ['./contactme.component.scss']
 })
 export class ContactmeComponent {
+  // Define properties to bind to the form
+  userName: string = '';
+  userEmail: string = '';
+  message: string = '';
+
+  // Function to send the email
   public sendEmail(e: Event) {
     e.preventDefault();
 
+    // Prepare the template parameters to match the template variable names
+    const templateParams = {
+      from_name: this.userName,  // from_name should be the sender's name
+      user_email: this.userEmail, // Optionally send the user's email
+      message: this.message       // The message content
+    };
+
+    // Send the email using emailjs
     emailjs
-      .sendForm('service_4uogd5r', 'template_ecnoiey', e.target as HTMLFormElement, {
-        publicKey: 'aZH5_9HUvSDxayVvW',
-      })
+      .send('service_4uogd5r', 'template_ecnoiey', templateParams, 'aZH5_9HUvSDxayVvW')
       .then(
         () => {
           alert('Success! Your message has been sent.');
@@ -24,7 +36,7 @@ export class ContactmeComponent {
         (error) => {
           alert('Failed to send the message. Please try again later.');
           console.log('FAILED...', (error as EmailJSResponseStatus).text);
-        },
+        }
       );
   }
 }
